91882c7ea1d586a374beb6a30b5732e5b3839137
Wendy Kim <wendykimm@berkeley.edu>
2022-02-14 20:26:15 +0000

    first commit

---
+++ pair.py
@@ -0,0 +1,31 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.11.5
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# %% [markdown]
+# ### to be paired
+
+# %%
+import numpy as np
+
+# %%
+exponents = np.arange(1, 50, 1)
+
+# %%
+2**exponents
+
+# %%
+print('exponents!')
+
+# %%
